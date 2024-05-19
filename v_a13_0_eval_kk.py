0eb610ef64d9eb6f63e102b52d917a28fac99caa
むずでょ <muzudho1@gmail.com>
2024-05-19 20:48:47 +0900

    WIP

--- v_a13_0_eval_kk.py
+++ v_a13_0_eval_kk.py
@@ -94,26 +94,126 @@ class EvaluationKkTable():
 
 
     _relative_sq_to_move_index = {
-        -10: 0,
-        -9: 1,
-        -8: 2,
-        -1: 3,
-        1: 4,
-        8: 5,
-        9: 6,
-        10: 7,
+        'A':{
+            1: 0,
+            9: 1,
+            10: 2,
+        },
+        'B':{
+            -1: 0,
+            1: 1,
+            8: 2,
+            9: 3,
+            10: 4,
+        },
+        'C':{
+            -1: 0,
+            8: 1,
+            9: 2,
+        },
+        'D':{
+            -9: 0,
+            -8: 1,
+            1: 2,
+            9: 3,
+            10: 4,
+        },
+        'E':{
+            -10: 0,
+            -9: 1,
+            -8: 2,
+            -1: 3,
+            1: 4,
+            8: 5,
+            9: 6,
+            10: 7,
+        },
+        'F':{
+            -10: 0,
+            -9: 1,
+            -1: 2,
+            8: 3,
+            9: 4,
+        },
+        'G':{
+            -9: 0,
+            -8: 1,
+            1: 2,
+        },
+        'H':{
+            -10: 0,
+            -9: 1,
+            -8: 2,
+            -1: 3,
+            1: 4,
+        },
+        'I':{
+            -10: 0,
+            -9: 1,
+            -1: 2,
+        },
     }
     """相対SQを、玉の指し手のインデックスへ変換"""
 
-    _e_relative_index_to_relative_sq = {
-        0: -10,
-        1: -9,
-        2: -8,
-        3: -1,
-        4: 1,
-        5: 8,
-        6: 9,
-        7: 10,
+    _relative_index_to_relative_sq = {
+        'A':{
+            0:1,
+            1:9,
+            2:10,
+        },
+        'B':{
+            0:-1,
+            1:1,
+            2:8,
+            3:9,
+            4:10,
+        },
+        'C':{
+            0:-1,
+            1:8,
+            2:9,
+        },
+        'D':{
+            0:-9,
+            1:-8,
+            2:1,
+            3:9,
+            4:10,
+        },
+        'E':{
+            0: -10,
+            1: -9,
+            2: -8,
+            3: -1,
+            4: 1,
+            5: 8,
+            6: 9,
+            7: 10,
+        },
+        'F':{
+            0:-10,
+            1:-9,
+            2:-1,
+            3:8,
+            4:9,
+        },
+        'G':{
+            0:-9,
+            1:-8,
+            2:1,
+        },
+        'H':{
+            0:-10,
+            1:-9,
+            2:-8,
+            3:-1,
+            4:1,
+        },
+        'I':{
+            0:-10,
+            1:-9,
+            2:-1,
+        },
     }
     """玉の指し手のインデックスを相対SQへ変換"""
 
@@ -211,7 +311,7 @@ class EvaluationKkTable():
         relative_sq = move_obj.dst_sq - src_sq
 
         try:
-            relative_index = EvaluationKkTable._relative_sq_to_move_index[relative_sq]
+            relative_index = EvaluationKkTable._relative_sq_to_move_index['E'][relative_sq]
 
         except KeyError as ex:
             # move_obj.as_usi:5a5b / relative_sq:1 move_obj.dst_sq:37 src_sq:36
@@ -425,7 +525,7 @@ class EvaluationKkTable():
         src_sq = rest
 
         # Ｅブロック
-        relative_sq = EvaluationKkTable._e_relative_index_to_relative_sq[relative_index]
+        relative_sq = EvaluationKkTable._relative_index_to_relative_sq['E'][relative_index]
         dst_sq = src_sq + relative_sq
 
         return Move.from_src_dst_pro(
