60aeb23adfd5fab40ef54acd6e616aae8c044aba
むずでょ <muzudho1@gmail.com>
2024-05-26 21:54:13 +0900

    WIP

---
+++ v_a20_0_eval_k.py
@@ -0,0 +1,121 @@
+class EvaluationKMove():
+    """自玉の指し手"""
+
+    _k_index_by_relative_sq = {
+        'A':{
+            1: 0,
+            9: 1,
+            10: 2,
+        },
+        'B':{
+            -1: 0,
+            1: 1,
+            8: 2,
+            9: 3,
+            10: 4,
+        },
+        'C':{
+            -1: 0,
+            8: 1,
+            9: 2,
+        },
+        'D':{
+            -9: 0,
+            -8: 1,
+            1: 2,
+            9: 3,
+            10: 4,
+        },
+        'E':{
+            -10: 0,
+            -9: 1,
+            -8: 2,
+            -1: 3,
+            1: 4,
+            8: 5,
+            9: 6,
+            10: 7,
+        },
+        'F':{
+            -10: 0,
+            -9: 1,
+            -1: 2,
+            8: 3,
+            9: 4,
+        },
+        'G':{
+            -9: 0,
+            -8: 1,
+            1: 2,
+        },
+        'H':{
+            -10: 0,
+            -9: 1,
+            -8: 2,
+            -1: 3,
+            1: 4,
+        },
+        'I':{
+            -10: 0,
+            -9: 1,
+            -1: 2,
+        },
+    }
+    """玉の指し手の相対SQを、インデックスへ変換"""
+
+
+    @staticmethod
+    def get_king_direction_max_number():
+        """玉の移動方向の最大数
+
+        Returns
+        -------
+        - int
+        """
+        return 8
+
+
+    @classmethod
+    def get_k_index_by_relative_sq(clazz):
+        """玉の指し手の相対SQを、インデックスへ変換するテーブルを取得"""
+        return clazz._k_index_by_relative_sq
+
+
+    @staticmethod
+    def get_index_by_k_move(
+            move_obj):
+        """指し手を指定すると、指し手のインデックスを返す。
+        ＫＫ評価値テーブル用
+
+        Parameters
+        ----------
+        move_obj : Move
+            指し手
+
+        Returns
+        -------
+            - 指し手のインデックス
+        """
+
+        # 移動元マス番号
+        #
+        #   - 打はありません。したがって None にはなりません
+        #
+        src_sq = move_obj.src_sq_or_none
+
+        # 玉は成らない
+
+        # 相対SQ    =  移動先マス番号    - 移動元マス番号
+        relative_sq = move_obj.dst_sq - src_sq
+
+        try:
+            k_index = EvaluationKMove.get_k_index_by_relative_sq()['E'][relative_sq]
+
+        except KeyError as ex:
+            # move_obj.as_usi:5a5b / relative_sq:1 move_obj.dst_sq:37 src_sq:36
+            print(f"move_obj.as_usi:{move_obj.as_usi} / relative_sq:{relative_sq} move_obj.dst_sq:{move_obj.dst_sq} src_sq:{src_sq}")
+            raise
+
+
+        # 0～647 =  0～80  *                                              8 +    0～7
+        return     src_sq * EvaluationKMove.get_king_direction_max_number() + k_index
