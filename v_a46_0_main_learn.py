00456651b25d49cfbcabe2eccd6c37b61e55b3a0
むずでょ <muzudho1@gmail.com>
2024-06-08 20:30:13 +0900

    learning framework

--- v_a46_0_main_learn.py
+++ v_a46_0_main_learn.py
@@ -5,20 +5,65 @@ from v_a46_0_misc.learn import Learn
 from v_a46_0_misc.lib import BoardHelper
 
 
-class MainLearn():
-    """学習部"""
+class LearningFramework():
+    """学習フレームワーク"""
 
 
     def __init__(self):
         pass
 
 
-    def learn_it(
+    def learn_about_one_game(
             self,
             board,
             kifuwarabe,
             is_debug):
-        """学習する
+        """対局１つについて学習する
+
+        Parameters
+        ----------
+        board : cshogi.Board
+            現局面
+        kifuwarabe : Kifuwarabe
+            きふわらべ
+        is_debug : bool
+            デバッグモードか？
+        """
+
+        # トレーニングデータの用意
+        print(f"[{datetime.datetime.now()}] [learning framework > learn about one game > get training data] start...")
+
+        # トレーニングデータが無いからプレイアウトする
+        result_str = kifuwarabe.playout(
+                is_debug=is_debug)
+
+        position_command = BoardHelper.get_position_command(
+                board=board)
+
+        print(f"""[{datetime.datetime.now()}] [learning framework > learn about one game > get training data] finished
+{board}
+    # playout result:`{result_str}`
+    # move_number:{board.move_number} / max_move_number:{max_move_number}
+    # {position_command}
+""", flush=True)
+
+        print(f"[{datetime.datetime.now()}] [learning framework > learn about one game] start...")
+
+        # 学習する
+        Learn(
+                board=board,
+                kifuwarabe=kifuwarabe,
+                is_debug=is_debug).learn_it()
+
+        print(f"[{datetime.datetime.now()}] [learning framework > learn about one game] finished")
+
+
+    def start_it(
+            self,
+            board,
+            kifuwarabe,
+            is_debug):
+        """学習フレームワークを始める
 
         Parameters
         ----------
@@ -33,32 +78,18 @@ class MainLearn():
         # 評価値テーブルの読込
         kifuwarabe.load_eval_all_tables()
 
-        # トレーニングデータの用意
-        print(f"[{datetime.datetime.now()}] [main learn > learn it > get training data] start...")
+        # とりあえず 2000対局について学習するか
+        max_game = 2000
+        for game_number in range(0,max_game):
 
-        # トレーニングデータが無いからプレイアウトする
-        result_str = kifuwarabe.playout(
-                is_debug=is_debug)
+            print(f"[{datetime.datetime.now()}] [learning framework > start it] ({game_number + 1:4}/{max_game}) start...")
 
-        position_command = BoardHelper.get_position_command(
-                board=board)
+            self.learn_about_one_game(
+                    board=board,
+                    kifuwarabe=kifuwarabe,
+                    is_debug=is_debug)
 
-        print(f"""[{datetime.datetime.now()}] [main learn > learn it > get training data] finished
-{board}
-    # playout result:`{result_str}`
-    # move_number:{board.move_number} / max_move_number:{max_move_number}
-    # {position_command}
-""", flush=True)
-
-        print(f"[{datetime.datetime.now()}] [main learn > learn it] start...")
-
-        # 学習する
-        Learn(
-                board=board,
-                kifuwarabe=kifuwarabe,
-                is_debug=is_debug).learn_it()
-
-        print(f"[{datetime.datetime.now()}] [main learn > learn it] finished")
+            print(f"[{datetime.datetime.now()}] [learning framework > start it] ({game_number + 1:4}/{max_game}) finished")
 
 
 ########################################
@@ -74,12 +105,12 @@ if __name__ == '__main__':
         kifuwarabe = Kifuwarabe()
         print(kifuwarabe.board)
 
-        main_learn = MainLearn()
-        main_learn.learn_it(
+        learning_framework = LearningFramework()
+        learning_framework.start_it(
                 board=kifuwarabe.board,
                 kifuwarabe=kifuwarabe,
                 is_debug=False)
 
     except Exception as err:
-        print(f"[{datetime.datetime.now()}] [main learn > unexpected error] {err=}, {type(err)=}")
+        print(f"[{datetime.datetime.now()}] [learning framework > unexpected error] {err=}, {type(err)=}")
         raise
