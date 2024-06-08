30dd07365f98c220257ea26dd76ce8944357256f
むずでょ <muzudho1@gmail.com>
2024-06-08 20:19:13 +0900

    学習フレームワーク

--- v_a46_0_main_learn.py
+++ v_a46_0_main_learn.py
@@ -1,7 +1,8 @@
 # python v_a46_0_main_learn.py
 import datetime
-from v_a46_0 import Kifuwarabe
+from v_a46_0 import Kifuwarabe, max_move_number
 from v_a46_0_misc.learn import Learn
+from v_a46_0_misc.lib import BoardHelper
 
 
 class MainLearn():
@@ -31,6 +32,23 @@ class MainLearn():
 
         # 評価値テーブルの読込
         kifuwarabe.load_eval_all_tables()
+
+        # トレーニングデータの用意
+        print(f"[{datetime.datetime.now()}] [main learn > learn it > get training data] start...")
+
+        # トレーニングデータが無いからプレイアウトする
+        result_str = kifuwarabe.playout(
+                is_debug=is_debug)
+
+        position_command = BoardHelper.get_position_command(
+                board=board)
+
+        print(f"""[{datetime.datetime.now()}] [main learn > learn it > get training data] finished
+{board}
+    # playout result:`{result_str}`
+    # move_number:{board.move_number} / max_move_number:{max_move_number}
+    # {position_command}
+""", flush=True)
 
         print(f"[{datetime.datetime.now()}] [main learn > learn it] start...")
 
