7ca1c7c970185247c87bc775590ed8022ef3ade1
むずでょ <muzudho1@gmail.com>
2024-06-09 22:15:54 +0900

    v_a52_0

---
+++ v_a52_0_eval/k.py
@@ -0,0 +1,451 @@
+from v_a52_0_misc.lib import MoveSourceLocation, MoveDestinationLocation, Move, BoardHelper
+
+
+class EvaluationKMove():
+    """自玉の指し手
+
+    下図：　玉の指し手は、現在地 you と、移動先の８方向で表せる
+
+    +----+----+----+
+    |  5 |  3 |  0 |
+    |    |    |    |
+    +----+----+----+
+    |  6 | You|  1 |
+    |    |    |    |
+    +----+----+----+
+    |  7 |  4 |  2 |
+    |    |    |    |
+    +----+----+----+
+
+    👆　例えば　5g5f　なら、移動先は 3 が該当する
+
+    👇　移動先は、移動元との相対SQを使うことでマス番号を取得できる
+
+    +----+----+----+
+    | +8 | -1 |-10 |
+    |    |    |    |
+    +----+----+----+
+    | +9 | You| -9 |
+    |    |    |    |
+    +----+----+----+
+    |+10 | +1 | -8 |
+    |    |    |    |
+    +----+----+----+
+
+    しかし、盤の隅に玉があるとき、玉は３方向にしか移動できないので、８方向のテーブルには無駄ができる。
+    切り詰めたいので　下図：９つのケース（Ａ～Ｉブロック）　に対応する９つのテーブルを用意する
+
+      9     2～8  1
+    +--+--------+--+
+    | G|      D | A| 一
+    +--+--------+--+
+    |  |        |  | 二～八
+    | H|      E | B|
+    |  |        |  |
+    +--+--------+--+
+    | I|      F | C| 九
+    +--+--------+--+
+
+    +----+----+    +----+----+----+    +----+----+
+    |  G |  0 |    |  3 |  D |  0 |    |  1 |  A |
+    |    |    |    |    |    |    |    |    |    |
+    +----+----+    +----+----+----+    +----+----+
+    |  2 |  1 |    |  4 |  2 |  1 |    |  2 |  0 |
+    |    |    |    |    |    |    |    |    |    |
+    +----+----+    +----+----+----+    +----+----+
+
+    +----+----+    +----+----+----+    +----+----+
+    |  3 |  0 |    |  5 |  3 |  0 |    |  2 |  0 |
+    |    |    |    |    |    |    |    |    |    |
+    +----+----+    +----+----+----+    +----+----+
+    |  H |  1 |    |  6 |  E |  1 |    |  3 |  B |
+    |    |    |    |    |    |    |    |    |    |
+    +----+----+    +----+----+----+    +----+----+
+    |  4 |  2 |    |  7 |  4 |  2 |    |  4 |  1 |
+    |    |    |    |    |    |    |    |    |    |
+    +----+----+    +----+----+----+    +----+----+
+
+    +----+----+    +----+----+----+    +----+----+
+    |  2 |  0 |    |  3 |  2 |  0 |    |  1 |  0 |
+    |    |    |    |    |    |    |    |    |    |
+    +----+----+    +----+----+----+    +----+----+
+    |  I |  1 |    |  4 |  F |  1 |    |  2 |  C |
+    |    |    |    |    |    |    |    |    |    |
+    +----+----+    +----+----+----+    +----+----+
+
+    Ａは３マス、　１か所、計　　３マス
+    Ｂは５マス、　７か所、計　３５マス
+    Ｃは３マス、　１か所、計　　３マス
+    Ｄは５マス、　７か所、計　３５マス
+    Ｅは８マス、４９か所、計３９２マス
+    Ｆは５マス、　７か所、計　３５マス
+    Ｇは３マス、　１か所、計　　３マス
+    Ｈは５マス、　７か所、計　３５マス
+    Ｉは３マス、　１か所、計　　３マス
+    合計　５４４マス
+
+    圧縮しない場合　８マス×８１か所で６４８マスだったので
+    ６４８ー５４４＝１０４マスの削減
+    1 - 544 / 658 = 0.17... なので１７％の削減
+    """
+
+    _src_to_dst_index_dictionary = None
+    """元マスと移動先マスを渡すと、マスの通し番号を返す入れ子の辞書"""
+
+    _index_to_src_dst_dictionary = None
+    """マスの通し番号を渡すと、元マスと移動先マスを返す入れ子の辞書"""
+
+
+    @classmethod
+    def get_src_sq_to_dst_sq_index_dictionary_tuple(clazz):
+        """元マスと移動先マスを渡すと、マスの通し番号を返す入れ子の辞書を返します。
+        初回アクセス時はテーブル生成に時間がかかります"""
+
+        # 未生成なら生成（重い処理は１回だけ）
+        if clazz._src_to_dst_index_dictionary == None:
+            # 右
+            right_file = - 1
+            # 左
+            left_file = 1
+            # 上
+            top_rank = - 1
+            # 下
+            bottom_rank = 1
+
+            # 順方向
+            clazz._src_to_dst_index_dictionary = dict()
+            # 逆方向
+            clazz._index_to_src_dst_dictionary = dict()
+
+            effect_serial_index = 0
+
+            #
+            # 移動元マス（１一～９九）
+            #
+            for src_sq in range(0,81):
+
+                dst_to_index_dictionary = dict()
+                clazz._src_to_dst_index_dictionary[src_sq] = dst_to_index_dictionary
+
+                # 利きのマスの集合
+                dst_sq_set = set()
+
+                # 元マスの座標
+                (src_file,
+                src_rank) = BoardHelper.get_file_rank_by_sq(src_sq)
+
+                #
+                # 絶対マス番号を作成
+                #
+
+                # 右上
+                dst_file = src_file + right_file
+                dst_rank = src_rank + top_rank
+                if 0 <= dst_file and 0 <= dst_rank:
+                    dst_sq_set.add(BoardHelper.get_sq_by_file_rank(
+                            file=dst_file,
+                            rank=dst_rank))
+
+                # 右
+                dst_file = src_file + right_file
+                dst_rank = src_rank
+                if 0 <= dst_file:
+                    dst_sq_set.add(BoardHelper.get_sq_by_file_rank(
+                            file=dst_file,
+                            rank=dst_rank))
+
+                # 右下
+                dst_file = src_file + right_file
+                dst_rank = src_rank + bottom_rank
+                if 0 <= dst_file and dst_rank < 9:
+                    dst_sq_set.add(BoardHelper.get_sq_by_file_rank(
+                            file=dst_file,
+                            rank=dst_rank))
+
+                # 上
+                dst_file = src_file
+                dst_rank = src_rank + top_rank
+                if 0 <= dst_rank:
+                    dst_sq_set.add(BoardHelper.get_sq_by_file_rank(
+                            file=dst_file,
+                            rank=dst_rank))
+
+                # 下
+                dst_file = src_file
+                dst_rank = src_rank + bottom_rank
+                if dst_rank < 9:
+                    dst_sq_set.add(BoardHelper.get_sq_by_file_rank(
+                            file=dst_file,
+                            rank=dst_rank))
+
+                # 左上
+                dst_file = src_file + left_file
+                dst_rank = src_rank + top_rank
+                if dst_file < 9 and 0 <= dst_rank:
+                    dst_sq_set.add(BoardHelper.get_sq_by_file_rank(
+                            file=dst_file,
+                            rank=dst_rank))
+
+                # 左
+                dst_file = src_file + left_file
+                dst_rank = src_rank
+                if dst_file < 9:
+                    dst_sq_set.add(BoardHelper.get_sq_by_file_rank(
+                            file=dst_file,
+                            rank=dst_rank))
+
+                # 左下
+                dst_file = src_file + left_file
+                dst_rank = src_rank + bottom_rank
+                if dst_file < 9 and dst_rank < 9:
+                    dst_sq_set.add(BoardHelper.get_sq_by_file_rank(
+                            file=dst_file,
+                            rank=dst_rank))
+
+                #
+                # マス番号を昇順に並べ替える
+                #
+                dst_sq_list = sorted(list(dst_sq_set))
+
+                #
+                # 左表の利きのマスに、通し番号を振っていく
+                #
+                for dst_sq in dst_sq_list:
+                    #print(f"[昇順] dst_sq={dst_sq}")
+
+                    dst_to_index_dictionary[dst_sq] = effect_serial_index
+                    clazz._index_to_src_dst_dictionary[effect_serial_index] = (src_sq, dst_sq)
+
+                    effect_serial_index += 1
+
+            # 玉に打は無い
+            ##
+            ## 打
+            ##
+            ##   移動元マス番号 81～87 に、持ち駒の種類　飛～歩　を当てる
+            ##
+            #for src_drop in MoveSourceLocation.get_drops():
+            #    # 飛
+            #    if src_drop == 'R*':
+            #        src_sq=81
+            #        dst_to_index_dictionary = dict()
+            #        clazz._src_to_dst_index_dictionary[src_sq] = dst_to_index_dictionary
+            #
+            #        # 打ち込み先マス（１一～９九）
+            #        for dst_sq in range(0,81):
+            #            dst_to_index_dictionary[dst_sq] = effect_serial_index
+            #            clazz._index_to_src_dst_dictionary[effect_serial_index] = (src_sq, dst_sq)
+            #
+            #            effect_serial_index += 1
+            #
+            #    # 角
+            #    elif src_drop == 'B*':
+            #        src_sq=82
+            #        dst_to_index_dictionary = dict()
+            #        clazz._src_to_dst_index_dictionary[src_sq] = dst_to_index_dictionary
+            #
+            #        # 打ち込み先マス（１一～９九）
+            #        for dst_sq in range(0,81):
+            #            dst_to_index_dictionary[dst_sq] = effect_serial_index
+            #            clazz._index_to_src_dst_dictionary[effect_serial_index] = (src_sq, dst_sq)
+            #
+            #            effect_serial_index += 1
+            #
+            #    # 金
+            #    elif src_drop == 'G*':
+            #        src_sq=83
+            #        dst_to_index_dictionary = dict()
+            #        clazz._src_to_dst_index_dictionary[src_sq] = dst_to_index_dictionary
+            #
+            #        # 打ち込み先マス（１一～９九）
+            #        for dst_sq in range(0,81):
+            #            dst_to_index_dictionary[dst_sq] = effect_serial_index
+            #            clazz._index_to_src_dst_dictionary[effect_serial_index] = (src_sq, dst_sq)
+            #
+            #            effect_serial_index += 1
+            #
+            #    # 銀
+            #    elif src_drop == 'S*':
+            #        src_sq=84
+            #        dst_to_index_dictionary = dict()
+            #        clazz._src_to_dst_index_dictionary[src_sq] = dst_to_index_dictionary
+            #
+            #        # 打ち込み先マス（１一～９九）
+            #        for dst_sq in range(0,81):
+            #            dst_to_index_dictionary[dst_sq] = effect_serial_index
+            #            clazz._index_to_src_dst_dictionary[effect_serial_index] = (src_sq, dst_sq)
+            #
+            #            effect_serial_index += 1
+            #
+            #    # 桂
+            #    elif src_drop == 'N*':
+            #        src_sq=85
+            #        dst_to_index_dictionary = dict()
+            #        clazz._src_to_dst_index_dictionary[src_sq] = dst_to_index_dictionary
+            #
+            #        # 打ち込み先マス（１一～９九）
+            #        for dst_sq in range(0,81):
+            #            dst_rank_th = dst_sq % 9 + 1
+            #            if dst_rank_th in [1,2]:
+            #                continue
+            #
+            #            dst_to_index_dictionary[dst_sq] = effect_serial_index
+            #            clazz._index_to_src_dst_dictionary[effect_serial_index] = (src_sq, dst_sq)
+            #
+            #            effect_serial_index += 1
+            #
+            #    # 香
+            #    elif src_drop == 'L*':
+            #        src_sq=86
+            #        dst_to_index_dictionary = dict()
+            #        clazz._src_to_dst_index_dictionary[src_sq] = dst_to_index_dictionary
+            #
+            #        # 打ち込み先マス（１一～９九）
+            #        for dst_sq in range(0,81):
+            #            dst_rank_th = dst_sq % 9 + 1
+            #            if dst_rank_th == 1:
+            #                continue
+            #
+            #            dst_to_index_dictionary[dst_sq] = effect_serial_index
+            #            clazz._index_to_src_dst_dictionary[effect_serial_index] = (src_sq, dst_sq)
+            #
+            #            effect_serial_index += 1
+            #
+            #    # 歩
+            #    elif src_drop == 'L*':
+            #        src_sq=87
+            #        dst_to_index_dictionary = dict()
+            #        clazz._src_to_dst_index_dictionary[src_sq] = dst_to_index_dictionary
+            #
+            #        # 打ち込み先マス（１一～９九）
+            #        for dst_sq in range(0,81):
+            #            dst_rank_th = dst_sq % 9 + 1
+            #            if dst_rank_th == 1:
+            #                continue
+            #
+            #            dst_to_index_dictionary[dst_sq] = effect_serial_index
+            #            clazz._index_to_src_dst_dictionary[effect_serial_index] = (src_sq, dst_sq)
+            #
+            #            effect_serial_index += 1
+            #
+            #    else:
+            #        raise ValueError(f"[evaluation k move > get src sq to dst sq index dictionary tuple] unexpected src drop:`{src_drop}`")
+
+
+        return (clazz._src_to_dst_index_dictionary, clazz._index_to_src_dst_dictionary)
+
+
+    def get_serial_number_size():
+        """玉の指し手の数
+
+        Returns
+        -------
+        - int
+        """
+        return 544
+
+
+    #get_index_of_k_move
+    @staticmethod
+    def get_index_by_k_move(
+            k_move_obj,
+            is_rotate):
+        """玉の指し手を指定すると、玉の指し手のインデックスを返す。
+
+        Parameters
+        ----------
+        k_move_obj : Move
+            玉の指し手
+        is_rotate : bool
+            後手なら真。指し手を１８０°回転させます
+
+        Returns
+        -------
+            - 玉の指し手のインデックス
+        """
+
+        if is_rotate:
+            k_src_sq_or_none = k_move_obj.src_location.rot_sq
+            k_dst_sq = k_move_obj.dst_location.rot_sq
+
+        else:
+            k_src_sq_or_none = k_move_obj.src_location.sq
+            k_dst_sq = k_move_obj.dst_location.sq
+
+        # 玉は成らない
+
+        # 元マスと移動先マスを渡すと、マスの通し番号を返す入れ子の辞書を返します
+        (src_to_dst_index_dictionary, _) = EvaluationKMove.get_src_sq_to_dst_sq_index_dictionary_tuple()
+
+        try:
+            # 移動元マス番号
+            #
+            #   - TODO 評価値テーブルに打は作ってるか？ k_src_sq_or_none は None になってるから k_src_drop_or_none がほしい？
+            #
+            dst_to_index_dictionary = src_to_dst_index_dictionary[k_src_sq_or_none]
+
+        except KeyError as ex:
+            print(f"k_move_obj.as_usi:{k_move_obj.as_usi}  rotated:{is_rotate}  k_src_sq:{k_src_sq_or_none}  src_masu:{BoardHelper.sq_to_jsa(k_src_sq_or_none)}  ex:{ex}")
+            raise
+
+        try:
+            # 移動元マス番号
+            #
+            #   - 打はありません。したがって None にはなりません
+            #
+            k_index = dst_to_index_dictionary[k_dst_sq]
+
+        except KeyError as ex:
+            # k_move_obj.as_usi:5a5b  src_sq:36  dst_sq:37
+            # k_move_obj.as_usi:5a4b  is_rotate:True  src_sq:44  dst_sq:52  src_masu:59  dst_masu:68  ex:28
+            # k_move_obj.as_usi:6g4e  rotated:True  k_src_sq:29  k_dst_sq:49  src_masu:43  dst_masu:65  ex:49
+            print(f"k_move_obj.as_usi:{k_move_obj.as_usi}  rotated:{is_rotate}  len(dst_to_index_dictionary):{len(dst_to_index_dictionary)}  k_src_masu:{BoardHelper.sq_to_jsa(k_src_sq_or_none)}  k_dst_masu:{BoardHelper.sq_to_jsa(k_dst_sq)}  k_src_sq:{k_src_sq_or_none}  k_dst_sq:{k_dst_sq}  ex:{ex}")
+            raise
+
+        # assert
+        if EvaluationKMove.get_serial_number_size() <= k_index:
+            raise ValueError(f"k_index:{k_index} out of range {EvaluationKMove.get_serial_number_size()}")
+
+        return k_index
+
+
+    @staticmethod
+    def destructure_k_index(
+            k_index,
+            is_rotate):
+        """Ｋインデックス分解
+
+        Parameter
+        ---------
+        k_index : int
+            玉の指し手のインデックス
+        is_rotate : bool
+            後手なら真。指し手を１８０°回転させます
+
+        Returns
+        -------
+        - k_move_obj : Move
+            玉の指し手
+        """
+
+        # マスの通し番号を渡すと、元マスと移動先マスを返す入れ子の辞書を返します
+        (_, index_to_src_dst_dictionary) = EvaluationKMove.get_src_sq_to_dst_sq_index_dictionary_tuple()
+
+        (src_sq, dst_sq) = index_to_src_dst_dictionary[k_index]
+
+        k_move_obj = Move.from_src_dst_pro(
+                src_location=MoveSourceLocation.from_sq_or_drop(
+                        sq_or_drop=src_sq),
+                dst_location=MoveDestinationLocation.from_sq(
+                        sq=dst_sq),
+                # 玉に成りはありません
+                promoted=False)
+
+        if is_rotate:
+            k_move_obj = Move.from_src_dst_pro(
+                    src_location=k_move_obj.src_location.rotate(),
+                    dst_location=k_move_obj.dst_location.rotate(),
+                    # 玉に成りはありません
+                    promoted=False)
+
+        return k_move_obj
---
+++ v_a52_0_test.py
@@ -0,0 +1,322 @@
+import cshogi
+import datetime
+
+# python v_a52_0_test.py
+from     v_a52_0_eval.k import EvaluationKMove
+from     v_a52_0_eval.kk import EvaluationKkTable
+from     v_a52_0_eval.p import EvaluationPMove
+from     v_a52_0_eval.pk import EvaluationPkTable
+from     v_a52_0_misc.bit_ope import BitOpe
+from     v_a52_0_misc.lib import Turn, MoveSourceLocation, MoveDestinationLocation, Move, BoardHelper
+from     v_a52_0_misc.debug import DebugHelper
+
+
+def test_k():
+    # 元マスと移動先マスを渡すと、マスの通し番号を返す入れ子の辞書を返します
+    (src_to_dst_index_dictionary, index_to_src_dst_dictionary) = EvaluationKMove.get_src_sq_to_dst_sq_index_dictionary_tuple()
+
+    base_name = "test_eval_k.log"
+    print(f"please read `{base_name}` file")
+
+    with open(base_name, 'w', encoding="utf-8") as f:
+
+        #
+        #
+        # １マスが３桁のテーブルを２つ並べる
+        #
+        #
+
+        #
+        # 元マス・先マス to インデックス
+        #
+        for src_sq in range(0,81):
+            dst_to_index_dictionary = src_to_dst_index_dictionary[src_sq]
+
+            #   - １マスが３桁の文字列の表
+            #   - 元マス
+            label_table_for_serial_index = ['   '] * 81
+            label_table_for_src_dst = ['   '] * 81
+
+            label_table_for_serial_index[src_sq] = 'you'
+            label_table_for_src_dst[src_sq] = 'you'
+
+            for dst_sq, serial_index in dst_to_index_dictionary.items():
+                label_table_for_serial_index[dst_sq] = f'{serial_index:3}'
+                label_table_for_src_dst[dst_sq] = f'{dst_sq:3}'
+
+            # 表示
+            f.write(f"""src_masu:{BoardHelper.sq_to_jsa(src_sq):2}
+src と dst                              通しインデックス
+{DebugHelper.stringify_double_3characters_boards(label_table_for_src_dst, label_table_for_serial_index)}
+""")
+
+        #
+        # インデックス to 元マス・先マス
+        #
+
+        previous_src_sq = -1
+
+        for serial_index in range(0, EvaluationKMove.get_serial_number_size()):
+            (src_sq, dst_sq) = index_to_src_dst_dictionary[serial_index]
+
+            print(f"(src_masu:{BoardHelper.sq_to_jsa(src_sq):2}, dst_masu:{BoardHelper.sq_to_jsa(dst_sq):2}) = dictionary[ serial_index:{serial_index:3} ]")
+
+            if previous_src_sq != src_sq:
+
+                if previous_src_sq != -1:
+                    # 表示
+                    f.write(f"""src_masu:{BoardHelper.sq_to_jsa(src_sq):2}
+通しインデックス                           src と dst
+{DebugHelper.stringify_double_3characters_boards(label_table_for_serial_index, label_table_for_src_dst)}
+""")
+
+
+                #   - １マスが３桁の文字列の表
+                #   - 元マス
+                label_table_for_src_dst = ['   '] * 81
+                label_table_for_src_dst[src_sq] = 'you'
+
+                label_table_for_serial_index = ['   '] * 81
+                label_table_for_serial_index[src_sq] = 'you'
+
+            label_table_for_src_dst[dst_sq] = f'{dst_sq:3}'
+            label_table_for_serial_index[dst_sq] = f'{serial_index:3}'
+
+            previous_src_sq = src_sq
+
+
+def test_kk():
+    k_move_obj_expected = Move.from_usi('5i5h')
+    l_move_obj_expected = Move.from_usi('5a5b')
+    k_turn = cshogi.BLACK
+
+    kk_index = EvaluationKkTable.get_index_of_kk_table(
+            k_move_obj=k_move_obj_expected,
+            l_move_obj=l_move_obj_expected,
+            k_turn=k_turn)
+
+    (k_move_obj_actual,
+     l_move_obj_actual) = EvaluationKkTable.destructure_kl_index(
+            kl_index=kk_index,
+            k_turn=k_turn)
+
+    if k_move_obj_expected.as_usi != k_move_obj_actual.as_usi:
+        raise ValueError(f"not match. k_turn:{Turn.to_string(k_turn)} K expected:`{k_move_obj_expected.as_usi}`  actual:`{k_move_obj_actual.as_usi}`")
+
+    if l_move_obj_expected.as_usi != l_move_obj_actual.as_usi:
+        raise ValueError(f"not match. k_turn:{Turn.to_string(k_turn)} L expected:`{l_move_obj_expected.as_usi}`  actual:`{l_move_obj_actual.as_usi}`")
+
+
+def test_p():
+    #
+    # 後手の歩を９段目に突いて成る
+    #
+    expected_p_move_u = '3h3i+'
+    expected_p_move_obj = Move.from_usi(expected_p_move_u)
+
+    p_index = EvaluationPMove.get_index_by_p_move(
+            p_move_obj=expected_p_move_obj,
+            is_rotate=True)
+
+    actual_p_move_obj = EvaluationPMove.destructure_p_index(
+            p_index=p_index,
+            is_rotate=True)
+
+    if expected_p_move_obj.as_usi != actual_p_move_obj.as_usi:
+        raise ValueError(f'unexpected error. move_obj expected P:`{expected_p_move_obj.as_usi}`  actual P:`{actual_p_move_obj.as_usi}`')
+
+
+
+    #
+    # 元マスと移動先マスを渡すと、マスの通し番号を返す入れ子の辞書を返します
+    #
+    (src_sq_to_dst_sq_to_index_for_npsi_dictionary,
+     src_sq_to_dst_sq_to_index_for_psi_dictionary,
+     drop_to_dst_sq_index,
+     index_to_src_sq_dst_sq_promotion_dictionary) = EvaluationPMove.get_src_sq_to_dst_sq_index_dictionary_tuple()
+
+    base_name = "test_eval_p.log"
+    print(f"please read `{base_name}` file")
+
+    with open(base_name, 'w', encoding="utf-8") as f:
+
+        #
+        #
+        # １マスが４桁のテーブルを４つ並べる
+        #
+        #
+
+        #
+        # 元マス・先マス to インデックス
+        #
+        for src_sq in range(0,81):
+            dst_sq_to_index_for_npsi_dictionary = src_sq_to_dst_sq_to_index_for_npsi_dictionary[src_sq]
+            dst_sq_to_index_for_b_dictionary = src_sq_to_dst_sq_to_index_for_psi_dictionary[src_sq]
+
+            # 成らない指し手（no promote）の各マス　値：通しインデックス（serial index）
+            label_table_for_npsi = ["    "] * 81
+
+            # 成る指し手（promote）の各マス　値：通しインデックス
+            label_table_for_psi = ["    "] * 81
+
+            # 成らない指し手の各マス　値：絶対マス番号（sq）
+            label_table_for_npsq = ["    "] * 81
+
+            # 成る指し手の各マス　値：絶対マス番号
+            label_table_for_psq = ["    "] * 81
+
+            label_table_for_npsi[src_sq] = " you"
+            label_table_for_psi[src_sq] = " you"
+            label_table_for_npsq[src_sq] = " you"
+            label_table_for_psq[src_sq] = " you"
+
+            for dst_sq, effect_index in dst_sq_to_index_for_npsi_dictionary.items():
+                label_table_for_npsi[dst_sq] = f"{effect_index:4}"
+                label_table_for_npsq[dst_sq] = f"{dst_sq:4}"
+
+            for dst_sq, effect_index in dst_sq_to_index_for_b_dictionary.items():
+                label_table_for_psi[dst_sq] = f"{effect_index:4}"
+                label_table_for_psq[dst_sq] = f"{dst_sq:4}"
+
+
+            f.write(f"""src_masu:{BoardHelper.sq_to_jsa(src_sq)}
+先手成らず  通しインデックス                          先手成らず  絶対マス                                先手成り  通しインデックス                            先手成り  絶対マス
+{DebugHelper.stringify_quadruple_4characters_board(
+        a=label_table_for_npsi,
+        b=label_table_for_npsq,
+        c=label_table_for_psi,
+        d=label_table_for_psq)}
+
+""")
+
+        # 打
+        #
+        #   - 打は SFEN では駒種類毎に分かれている。 R*, B*, G*, S*, N*, L*, P*
+        #
+        for drop in ['R*', 'B*', 'G*', 'S*', 'N*', 'L*', 'P*']:
+            dst_sq_to_index_dictionary = drop_to_dst_sq_index[drop]
+
+            label_table_for_drop = ['    '] * 81
+
+            for dst_sq, effect_index in dst_sq_to_index_dictionary.items():
+                label_table_for_drop[dst_sq] = f"{effect_index:4}"
+
+            f.write(f"""
+drop:{drop}
+{DebugHelper.stringify_4characters_board(label_table_for_drop)}
+
+""")
+
+        # TODO 後手
+
+
+def test_pk():
+    # 後手
+    expected_p_move_u = '3h3i+'
+    expected_p_move_obj = Move.from_usi(expected_p_move_u)
+
+    # 先手
+    expected_k_move_u = '5h5i'
+    expected_k_move_obj = Move.from_usi(expected_k_move_u)
+
+    pk_index = EvaluationPkTable.get_index_of_pk_table(
+            p_move_obj=expected_p_move_obj,
+            k_move_obj=expected_k_move_obj,
+            p_turn=cshogi.WHITE)
+
+    (actual_p_move_obj,
+     actual_k_move_obj) = EvaluationPkTable.destructure_pk_index(
+            pk_index=pk_index,
+            p_turn=cshogi.WHITE)
+
+    if expected_p_move_obj.as_usi != actual_p_move_obj.as_usi or expected_k_move_obj.as_usi != actual_k_move_obj.as_usi:
+        raise ValueError(f'unexpected error. move_obj expected P:`{expected_p_move_obj.as_usi}`  K:`{expected_k_move_obj.as_usi}`  actual P:`{actual_p_move_obj.as_usi}`  K:`{actual_k_move_obj.as_usi}`')
+
+
+def test_bit_ope():
+    init_value = 0b0000_0001
+    bit_shift = 2
+    expected_value = 0b0000_0101
+    actual_value = BitOpe.stand_at(init_value, bit_shift)
+    print(f"[{datetime.datetime.now()}] {init_value:08b} <<< {bit_shift} ----> actual_value:0b{actual_value:08b}")
+    if actual_value != expected_value:
+        raise ValueError(f"actual:{actual_value}  expected:{expected_value}")
+
+    init_value = 0b0000_1000
+    bit_shift = 3
+    expected_value = 0b0000_1000
+    actual_value = BitOpe.stand_at(init_value, bit_shift)
+    print(f"[{datetime.datetime.now()}] {init_value:08b} <<< {bit_shift} ----> actual_value:0b{actual_value:08b}")
+    if actual_value != expected_value:
+        raise ValueError(f"actual:{actual_value}  expected:{expected_value}")
+
+    init_value = 0b0000_0111
+    bit_shift = 1
+    expected_value = 0b0000_0101
+    actual_value = BitOpe.sit_at(init_value, bit_shift)
+    print(f"[{datetime.datetime.now()}] {init_value:08b} <<< {bit_shift} ----> actual_value:0b{actual_value:08b}")
+    if actual_value != expected_value:
+        raise ValueError(f"actual:{actual_value}  expected:{expected_value}")
+
+
+    init_value = 0b1000_0000
+    bit_shift = 7
+    expected_value = 0b0000_0001
+    actual_value = BitOpe.get_bit_at(init_value, bit_shift)
+    print(f"[{datetime.datetime.now()}] ({init_value:08b} >>> {bit_shift}) % 2 ----> actual_value:0b{actual_value:08b}")
+    if actual_value != expected_value:
+        raise ValueError(f"actual:{actual_value}  expected:{expected_value}")
+
+
+def test_lib():
+    expected_move_u = '3h3i+'
+    move_obj = Move.from_usi(expected_move_u)
+    if move_obj.as_usi != expected_move_u:
+        raise ValueError(f'unexpected error. move_obj expected:`{expected_move_u}`  actual:`{move_obj.as_usi}`')
+
+    if not move_obj.promoted:
+        raise ValueError(f'unexpected error. move_obj.promoted expected:True  actual:`{move_obj.promoted}`')
+
+    move_obj = Move.from_src_dst_pro(
+            src_location=MoveSourceLocation.from_sq_or_drop(
+                sq=BoardHelper.jsa_to_sq(38)),
+            dst_location=MoveDestinationLocation.from_sq(
+                sq=BoardHelper.jsa_to_sq(39)),
+            promoted=True)
+    
+    if move_obj.as_usi != expected_move_u:
+        raise ValueError(f'unexpected error. expected:`{expected_move_u}`  actual:`{move_obj.as_usi}`')
+
+    if not move_obj.promoted:
+        raise ValueError(f'unexpected error. move_obj.promoted expected:True  actual:`{move_obj.promoted}`')
+
+
+########################################
+# スクリプト実行時
+########################################
+
+if __name__ == '__main__':
+    """スクリプト実行時"""
+
+    line = input('test name?')
+
+    if line == 'k':
+        test_k()
+
+    elif line == 'kk':
+        test_kk()
+
+    elif line == 'p':
+        test_p()
+
+    elif line == 'pk':
+        test_pk()
+
+    elif line == 'bit_ope':
+        test_bit_ope()
+
+    elif line == 'lib':
+        test_lib()
+
+    else:
+        print("please input test name 'k', ...")
